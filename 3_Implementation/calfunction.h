f8679cf2f4cb1f8734b9210a35d36326be7dd422
Roobini venkatachalam <94366047+40020851@users.noreply.github.com>
2021-11-22 17:37:17 +0530

    Add header  file

---
+++ 3_Implementation/calfunction.h
@@ -0,0 +1,28 @@
+#ifndef calfunction.h
+#define calfunction.h
+
+int isLeapYear( int year );        
+int leapYears( int year );         
+int todayOf( int y, int m, int d); 
+long days( int y, int m, int d);   
+void calendar(int y, int m);       
+int getDayNumber(int d,int m,int y);
+char *getName(int day);
+
+void flush()
+
+int isLeapYear( int y )
+ 
+int leapYears( int y )
+int todayOf( int y, int m, int d)
+ 
+long days( int y, int m, int d)
+ 
+void calendar(int y, int m)
+
+ 
+int getDayNumber(int d, int m, int y)
+ 
+char *getName(int day)
+
+#endif
