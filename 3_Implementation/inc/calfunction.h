7d024593e56e6ffa52bc26baac0effb00e1bcec3
Roobini venkatachalam <94366047+40020851@users.noreply.github.com>
2021-11-23 19:00:52 +0530

    Header file

---
+++ 3_Implementation/inc/calfunction.h
@@ -0,0 +1,31 @@
+#ifndef __CALFUNCTION_H__
+#define __CALFUNCTION_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int isLeapYear( int year );        
+int leapYears( int year );         
+int todayOf( int y, int m, int d); 
+long days( int y, int m, int d);   
+void calendar(int y, int m);       
+int getDayNumber(int d,int m,int y);
+char *getName(int day);
+
+void flush()
+
+int isLeapYear( int y )
+ 
+int leapYears( int y )
+int todayOf( int y, int m, int d)
+ 
+long days( int y, int m, int d)
+ 
+void calendar(int y, int m)
+
+ 
+int getDayNumber(int d, int m, int y)
+ 
+char *getName(int day)
+
+#endif
