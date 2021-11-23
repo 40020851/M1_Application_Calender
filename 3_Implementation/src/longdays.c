9e5b73a2cca36362c421812a48fb5a9b9f854f43
Roobini venkatachalam <94366047+40020851@users.noreply.github.com>
2021-11-23 20:49:31 +0530

    source file

---
+++ 3_Implementation/src/longdays.c
@@ -0,0 +1,13 @@
+#include <calfunction.h>
+
+int todayOf( int y, int m, int d) {
+    static int DayOfMonth[] = 
+        { -1,0,31,59,90,120,151,181,212,243,273,304,334};
+    return DayOfMonth[m] + d + ((m>2 && isLeapYear(y))? 1 : 0);
+}
+ 
+long days( int y, int m, int d){
+    int lastYear;
+    lastYear = y - 1;
+    return 365L * lastYear + leapYears(lastYear) + todayOf(y,m,d);
+}
