5cfb084aa0cb5e950bac70f776ec1eea0b03c486
Roobini venkatachalam <94366047+40020851@users.noreply.github.com>
2021-11-23 20:53:01 +0530

    source file

---
+++ 3_Implementation/src/getname.c
@@ -0,0 +1,14 @@
+#include <calfunction.h>
+
+char *getName(int day){ //returns the name of the day
+   switch(day){
+      case 0 :return("Sunday");
+      case 1 :return("Monday");
+      case 2 :return("Tuesday");
+      case 3 :return("Wednesday");
+      case 4 :return("Thursday");
+      case 5 :return("Friday");
+      case 6 :return("Saturday");
+      default:return("Error: Invalid Argument Passed");
+   }
+}
